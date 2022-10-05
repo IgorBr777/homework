d8535604296d0e1633ae6a3be465f92702d7ec3d
IgorBr777 <igorbraz14@gmail.com>
2022-10-05 21:14:32 +0300

    HomeWork6, created absract class Transport, class CreditCard and InterfaceCallbackCar

--- app/src/main/java/com/example/homework4/CreditCard.java
+++ app/src/main/java/com/example/homework4/CreditCard.java
@@ -5,9 +5,16 @@ public class CreditCard {
 
     public static void main(String[] args) {
         CreditCard firsCreditCard = new CreditCard(1, 1000);
-        CreditCard secondCreditCard = new CreditCard(1, 1000);
-        CreditCard thirdCreditCard = new CreditCard(1, 1000);
+        CreditCard secondCreditCard = new CreditCard(2, 500);
+        CreditCard thirdCreditCard = new CreditCard(3, 560);
 
+        firsCreditCard.chargeAmountCreditCard(144);
+        secondCreditCard.chargeAmountCreditCard(450);
+        thirdCreditCard.withdrawAmountCreditCard(60);
+
+        firsCreditCard.currentAmountCreditCard();
+        secondCreditCard.currentAmountCreditCard();
+        thirdCreditCard.currentAmountCreditCard();
     }
 
     private int accountNumber;
@@ -20,18 +27,18 @@ public class CreditCard {
 
     }
 
-    public void chargeAmountCreditCard(int currentAmount){
-
+    public void chargeAmountCreditCard(int addMoney){
+        currentAmount = currentAmount + addMoney;
     }
 
-    public void withdrawAmountCreditCard(){
 
+    public void withdrawAmountCreditCard (int sendMoney){
+        currentAmount = currentAmount - sendMoney;
     }
 
     public void currentAmountCreditCard(){
-
+        System.out.println("Номер карточки  " + accountNumber + "\n" + "Сумма " + currentAmount + "\n");
 
     }
-
 
 }
